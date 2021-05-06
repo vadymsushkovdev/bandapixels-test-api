0704d769bc4170dc279f0123afcf33ec12a5666b
Vadym Sushkov <vadymsushkovdev@gmail.com>
2021-05-06 18:33:51 +0300

    Services structure changed

--- src/components/User/interfaces/interface.ts
+++
@@ -1,8 +0,0 @@
-import { IUserModel } from '@components/User/models/model';
-
-export interface IUserService {
-
-    usersInfo(token: string): Promise < IUserModel >;
-
-    createUser(IUserModel: IUserModel): Promise < IUserModel > ;
-}
